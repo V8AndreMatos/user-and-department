1f898c3731c44b96c74d9fac5c076cf460b74336
Andre <andreluisv8@gmail.com>
2025-08-26 22:55:36 -0300

    Created import.sql file with seed database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_department (name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Tecnologia');
+
+INSERT INTO tb_user(department_id , name , email) VALUES(1 , 'Júlia'  , 'julia@email.com');
+INSERT INTO tb_user(department_id , name , email) VALUES(1 , 'Soraia' , 'soraia@email.com');
+INSERT INTO tb_user(department_id , name , email) VALUES(2 , 'André'  , 'andre@email.com');
+INSERT INTO tb_user(department_id , name , email) VALUES(2 , 'Nego'   , 'nego@email.com');
+INSERT INTO tb_user(department_id , name , email) VALUES(2 , 'Zeca'   , 'zeca@email.com');
